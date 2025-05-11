cf0437efceeb791905b4d39172501ec2d72a601d
oh-facts <as17793@nyu.edu>
2025-05-11 03:35:45 -0400

    basic 2d engine starting code

---
+++ shaders/sprite.glsl
@@ -0,0 +1,84 @@
+#version 450
+#extension GL_ARB_bindless_texture : enable
+
+uniform mat4 u_view;
+uniform mat4 u_proj;
+
+#define Corner_00 0
+#define Corner_01 1
+#define Corner_10 2
+#define Corner_11 3
+#define Corner_COUNT 4
+
+struct Rect
+{
+	vec2 tl;
+	vec2 br;
+};
+
+struct R_Rect2
+{
+	mat4 model;
+	vec4 color;
+	uvec2 tex_id;
+	uvec2 pad;
+};
+
+layout (std430, row_major, binding = 0) buffer ssbo2 
+{
+	R_Rect2 rects[];
+};
+
+#define COMMON_SHADER_END
+
+flat out int a_index;
+out vec2 a_uv;
+
+void main()
+{
+	R_Rect2 obj = rects[gl_InstanceID];
+	
+	vec2 vertices[] =
+	{
+		{-1.0, -1.0},
+		{ 1.0, -1.0},
+		{-1.0,  1.0},
+		
+		{-1.0,  1.0},
+		{ 1.0, -1.0},
+		{ 1.0,  1.0}
+	};
+	
+	vec2 base_uv[] = 
+	{
+		{0, 0},
+		{1, 0},
+		{0, 1},
+		
+		{0, 1},
+		{1, 0},
+		{1, 1},
+	};
+	
+	a_uv = base_uv[gl_VertexID];
+	
+	vec2 vertex = vertices[gl_VertexID];
+	vertex.x /= 2;
+	vertex.y /= 2;
+	
+	gl_Position = u_proj * u_view * obj.model * vec4(vertex, 0, 1);
+	a_index = gl_InstanceID;
+}
+
+#define VERTEX_SHADER_END
+
+layout (location=0) out vec4 out_color;
+flat in int a_index;
+in vec2 a_uv;
+
+void main()
+{
+	R_Rect2 obj = rects[a_index];
+	vec4 tex_col = texture(sampler2D(obj.tex_id), a_uv);
+	out_color = obj.color * tex_col;
+}
